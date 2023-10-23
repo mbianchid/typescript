2ee0812059072e2beefd41fc9b4a42325b26f2d9
Miguel Bianchi Dias <mbianchid.12@gmail.com>
2023-10-23 16:10:22 -0300

    add decorators and adjusting class Conta

--- curso-02/dist/js/types/Decorators.js
+++ curso-02/dist/js/types/Decorators.js
@@ -11,3 +11,13 @@ export function ValidaDebito(target, propertyKey, descriptor) {
     };
     return descriptor;
 }
+export function ValidaDeposito(target, propertyKey, descriptor) {
+    const originalMethod = descriptor.value;
+    descriptor.value = function (valorDoDeposito) {
+        if (valorDoDeposito <= 0) {
+            throw new Error("Ovalor a ser debitado deve ser maior que zero!");
+        }
+        return originalMethod.apply(this, [valorDoDeposito]);
+    };
+    return descriptor;
+}
--- curso-02/src/types/Decorators.ts
+++ curso-02/src/types/Decorators.ts
@@ -15,3 +15,16 @@ export function ValidaDebito(target: any, propertyKey: string, descriptor: Prope
 
     return descriptor;
 }
+
+export function ValidaDeposito(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function(valorDoDeposito: number) {
+        if (valorDoDeposito <= 0 ) {
+            throw new Error("Ovalor a ser debitado deve ser maior que zero!");
+        }
+        return originalMethod.apply(this, [valorDoDeposito]);
+    }
+
+    return descriptor;
+}
